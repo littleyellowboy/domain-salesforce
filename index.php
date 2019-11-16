d4527478912185c88e53c82cc94f506fc5106e28
Johnny <truong_johnny@hotmail.fr>
2019-11-16 19:11:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,1 @@
 <?php 
-
-$request = $_SERVER['REQUEST_URI'];
-
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/about.php';
-        break;
-    case '' :
-        require __DIR__ . '/views/about.php';
-        break;
-    case '/about' :
-        require __DIR__ . '/views/about.php';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
-}
