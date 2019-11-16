fc52d2aa4c182b286e2b33d7fa461cdc36b8a50a
Johnny <truong_johnny@hotmail.fr>
2019-11-16 18:50:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php 
-  get('1', function() { return 'Je suis la page 1 !'; });
-  get('2', function() { return 'Je suis la page 2 !'; });
-  get('3', function() { return 'Je suis la page 3 !'; });
-?>
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about' :
+        require __DIR__ . '/views/about.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
