21f451724baf9e7fae3eba7768f90b29fc24f16a
Johnny <truong_johnny@hotmail.fr>
2019-11-16 18:47:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("mydomain.html"); ?>
+<?php 
+  get('1', function() { return 'Je suis la page 1 !'; });
+  get('2', function() { return 'Je suis la page 2 !'; });
+  get('3', function() { return 'Je suis la page 3 !'; });
+?>
